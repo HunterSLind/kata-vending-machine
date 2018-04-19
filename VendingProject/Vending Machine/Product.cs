fd0dbf9057219b1db1d12ef2eaae1d7c2a4af2f2
HLind <hunterslind@gmail.com>
2018-04-18 19:30:08 -0500

    Added Product object.

---
+++ VendingProject/Vending Machine/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vending_Machine
+{
+    public class Product
+    {
+        public Product(string productName, int price, int id, int stock)
+        {
+            ProductName = productName;
+            Price = price;
+            ID = id;
+            Stock = stock;
+        }
+
+        public string ProductName { get; private set; }
+        public int Price { get; private set; }
+        public int ID { get; private set; }
+        public int Stock { get; private set; }
+
+        public bool DispenseItem()
+        {
+            if(Stock > 0)
+            {
+                Stock = Stock - 1;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
